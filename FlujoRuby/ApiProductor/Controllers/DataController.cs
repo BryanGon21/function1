96dabdec5e4e0300d29cb501a1c681044fa35394
BryanGonCa <bryangonca@gmail.com>
2021-03-16 02:28:15 -0400

    muchos cambios

---
+++ FlujoRuby/ApiProductor/Controllers/DataController.cs
@@ -0,0 +1,36 @@
+﻿namespace ApiProductor.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Threading.Tasks;
+
+    using Azure.Messaging.ServiceBus;
+    using ApiProductor.Models;
+    using Newtonsoft.Json;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<bool> EnviarAsync([FromBody] Data data)
+        {
+             string connectionString = "Endpoint=sb://queuebryan.servicebus.windows.net/;SharedAccessKeyName=Enviar;SharedAccessKey=qQ7S7g9V1PyvhJJbonR+zR2nlH8OBEMbm2FgIt+nGic=;EntityPath=cola1";
+             string queueName = "cola1";
+            string mensaje = JsonConvert.SerializeObject(data);
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+            return true;
+        } 
+    }
+}
